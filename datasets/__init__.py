d3bd9f306ff27ad775a94369d6c2484fedaf18c8
ziqi-jin <ziqijincoder@gmail.com>
2023-04-15 13:10:40 +0800

    modify dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,8 +1,11 @@
 from .detection import BaseDetectionDataset
 from .instance_seg import BaseInstanceDataset
 from .semantic_seg import BaseSemanticDataset
+
 name_dict = {'base_det': BaseDetectionDataset, 'base_ins': BaseInstanceDataset, 'base_sem': BaseSemanticDataset}
+
+
 def get_dataset(name, path):
     if name not in name_dict:
-        print('not supported dataset name, please implement first.')
+        print('not supported dataset name, please implement it first.')
     return name_dict[name](path)
