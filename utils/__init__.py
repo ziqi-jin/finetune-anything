a001c26a7f4f619a08294e1d01e116e4e79c6630
ziqi-jin <ziqijincoder@gmail.com>
2023-05-04 21:23:02 +0800

    add loss function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,13 @@
 import torch.nn as nn
+
 name_dict = {'ce_loss': nn.CrossEntropyLoss, 'multi_label_soft_margin': nn.MultiLabelSoftMarginLoss}
+
+
 def get_losses(loss_names):
     loss_dict = {}
     for name in loss_names:
         if name not in name_dict:
-            print('not supported dataset name, please implement it first.')
+            print('not supported loss name, please implement it first.')
             break
         loss_dict[name] = name_dict[name]
     if len(loss_dict) != len(loss_names):
