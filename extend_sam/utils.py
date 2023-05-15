1948816e8f7a511cd5372b18571256e2f5a32213
ziqi-jin <ziqijincoder@gmail.com>
2023-05-15 21:38:58 +0800

    add mIOU metrics

--- extend_sam/utils.py
+++ extend_sam/utils.py
@@ -88,3 +88,81 @@ def print_and_save_log(message, path):
     print(message)
     with open(path, 'a+') as f:
         f.write(message + '\n')
+
+
+class mIOUOnline:
+    def __init__(self, class_names):
+        self.class_names = class_names
+        self.classes = len(self.class_names)
+
+        self.clear()
+
+    def get_data(self, pred_mask, gt_mask):
+        obj_mask = gt_mask < 255
+        correct_mask = (pred_mask == gt_mask) * obj_mask
+
+        P_list, T_list, TP_list = [], [], []
+        for i in range(self.classes):
+            P_list.append(np.sum((pred_mask == i) * obj_mask))
+            T_list.append(np.sum((gt_mask == i) * obj_mask))
+            TP_list.append(np.sum((gt_mask == i) * correct_mask))
+
+        return (P_list, T_list, TP_list)
+
+    def add_using_data(self, data):
+        P_list, T_list, TP_list = data
+        for i in range(self.classes):
+            self.P[i] += P_list[i]
+            self.T[i] += T_list[i]
+            self.TP[i] += TP_list[i]
+
+    def add(self, pred_mask, gt_mask):
+        obj_mask = gt_mask < 255
+        correct_mask = (pred_mask == gt_mask) * obj_mask
+
+        for i in range(self.classes):
+            self.P[i] += np.sum((pred_mask == i) * obj_mask)
+            self.T[i] += np.sum((gt_mask == i) * obj_mask)
+            self.TP[i] += np.sum((gt_mask == i) * correct_mask)
+
+    def get(self, detail=False, clear=True):
+        IoU_dic = {}
+        IoU_list = []
+
+        FP_list = []  # over activation
+        FN_list = []  # under activation
+
+        for i in range(self.classes):
+            IoU = self.TP[i] / (self.T[i] + self.P[i] - self.TP[i] + 1e-10) * 100
+            FP = (self.P[i] - self.TP[i]) / (self.T[i] + self.P[i] - self.TP[i] + 1e-10)
+            FN = (self.T[i] - self.TP[i]) / (self.T[i] + self.P[i] - self.TP[i] + 1e-10)
+
+            IoU_dic[self.class_names[i]] = IoU
+
+            IoU_list.append(IoU)
+            FP_list.append(FP)
+            FN_list.append(FN)
+
+        mIoU = np.mean(np.asarray(IoU_list))
+        mIoU_foreground = np.mean(np.asarray(IoU_list)[1:])
+
+        FP = np.mean(np.asarray(FP_list))
+        FN = np.mean(np.asarray(FN_list))
+
+        if clear:
+            self.clear()
+
+        if detail:
+            return mIoU, mIoU_foreground, IoU_dic, FP, FN
+        else:
+            return mIoU, mIoU_foreground
+
+    def clear(self):
+        self.TP = []
+        self.P = []
+        self.T = []
+
+        for _ in range(self.classes):
+            self.TP.append(0)
+            self.P.append(0)
+            self.T.append(0)
