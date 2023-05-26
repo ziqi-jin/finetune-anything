95b8a9f0519cc80d4ad2ceb254457f51fa070ae1
ziqi-jin <ziqijincoder@gmail.com>
2023-05-26 16:59:15 +0800

    SemanticSam modify

--- extend_sam/extend_sam.py
+++ extend_sam/extend_sam.py
@@ -3,17 +3,17 @@ import torch
 import torch.nn as nn
 from segment_anything import sam_model_registry
 from .image_encoder_adapter import BaseImgEncodeAdapter
-from .mask_decoder_adapter import BaseMaskDecodeAdapter
+from .mask_decoder_adapter import BaseMaskDecoderAdapter, SemMaskDecoderAdapter
 from .prompt_encoder_adapter import BasePromptEncodeAdapter
 
 
 class BaseExtendSam(nn.Module):
 
     def __init__(self, ckpt_path=None, fix_img_en=False, fix_promt_en=False, fix_mask_de=False):
-        ori_sam = sam_model_registry['default'](ckpt_path)
+        self.ori_sam = sam_model_registry['default'](ckpt_path)
         self.img_adapter = BaseImgEncodeAdapter(ori_sam, fix=fix_img_en)
         self.prompt_adapter = BasePromptEncodeAdapter(ori_sam, fix=fix_promt_en)
-        self.mask_adapter = BaseMaskDecodeAdapter(ori_sam, fix=fix_mask_de)
+        self.mask_adapter = BaseMaskDecoderAdapter(ori_sam, fix=fix_mask_de)
 
     def forword(self, img):
         x = self.img_adapter(img)
@@ -23,5 +23,7 @@ class BaseExtendSam(nn.Module):
 
 
 class SemanticSam(BaseExtendSam):
-    def __init__(self):
-        pass
+    def __init__(self, ckpt_path=None, fix_img_en=False, fix_promt_en=False, fix_mask_de=False):
+        super().__init__(ckpt_path=ckpt_path, fix_img_en=fix_promt_en, fix_promt_en=False, fix_mask_de=fix_mask_de)
+        self.mask_adapter = SemMaskDecoderAdapter(self.ori_sam, fix=fix_img_en)
+
