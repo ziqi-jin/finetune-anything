4f8c008d4810ca4f4139857e15572cd2c6dbabce
ziqi-jin <ziqijincoder@gmail.com>
2023-06-08 19:03:52 +0800

    BaseSemanticDataset

--- datasets/semantic_seg.py
+++ datasets/semantic_seg.py
@@ -5,16 +5,63 @@ from torchvision.datasets import VOCSegmentation
 
 
 class BaseSemanticDataset(Dataset):
-    '''
-    image: input image
-    label: semantic mask with classes
-    '''
+    """
+    if you want to customize a new dataset to train the segmentation task,
+    the img and mask file need be arranged as this sturcture.
+        ├── data
+        │   ├── my_dataset
+        │   │   ├── img
+        │   │   │   ├── train
+        │   │   │   │   ├── xxx{img_suffix}
+        │   │   │   │   ├── yyy{img_suffix}
+        │   │   │   │   ├── zzz{img_suffix}
+        │   │   │   ├── val
+        │   │   ├── ann
+        │   │   │   ├── train
+        │   │   │   │   ├── xxx{ann_suffix}
+        │   │   │   │   ├── yyy{ann_suffix}
+        │   │   │   │   ├── zzz{ann_suffix}
+        │   │   │   ├── val
+    """
 
-    def __init__(self):
-        pass
+    def __init__(self, metainfo, dataset_dir,
+                 image_set='train',
+                 img_suffix='.jpg',
+                 ann_suffix='.png',
+                 data_prefix: dict = dict(img_path='img', ann_path='ann'), return_dict=False):
+        '''
 
-    def __getitem__(self, item):
-        pass
+        :param metainfo: meta data in original dataset, e.g. class_names
+        :param dataset_dir: the path of your dataset, e.g. data/my_dataset/ by the stucture tree above
+        :param image_set: 'train' or 'val'
+        :param img_suffix: your image suffix
+        :param ann_suffix: your annotation suffix
+        :param data_prefix: data folder name, as the tree shows above, the data_prefix of my_dataset: img_path='img' , ann_path='ann'
+        :param return_dict: return dict() or tuple(img, ann)
+        '''
+        self.class_names = metainfo['class_names']
+        self.img_path = os.path.join(dataset_dir, data_prefix['img_path'], image_set)
+        self.ann_path = os.path.join(dataset_dir, data_prefix['ann_path'], image_set)
+        print('img_folder_name: {img_folder_name}, ann_folder_name: {ann_folder_name}'.format(
+            img_folder_name=self.img_path, ann_folder_name=self.ann_path))
+        self.img_names = [img_name.split(img_suffix)[0] for img_name in os.listdir(self.img_path) if
+                          img_name.endswith(img_suffix)]
+        self.img_suffix = img_suffix
+        self.ann_suffix = ann_suffix
+        self.return_dict = return_dict
+
+    def __getitem__(self, index):
+        img = Image.open(os.path.join(self.img_path, self.img_names[index] + self.img_suffix))
+        ann = Image.open(os.path.join(self.ann_path, self.img_names[index] + self.ann_suffix))
+        if self.return_dict:
+            data = dict(img_name=self.img_names[index], img=img, ann=ann,
+                        img_path=os.path.join(self.img_path, self.img_names[index] + self.img_suffix),
+                        ann_path=os.path.join(self.ann_path, self.img_names[index] + self.ann_suffix))
+            return data
+        return img, ann
+
+    def __len__(self):
+        return len(self.img_names)
 
 
 class VOCSemanticDataset(Dataset):
